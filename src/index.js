a707089bfa4fa7b2c361d70565b8f628cef4ee2d
grayyeargin <grayyeargin@gmail.com>
2019-12-20 20:33:56 -0500

    tweak colors and add css play btn

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ const audioElement = document.querySelector('audio');
 audioElement.src = audioSrc;
 const track = audioCtx.createMediaElementSource(audioElement);
 const analyser = audioCtx.createAnalyser();
+analyser.fftSize = 128;
 
 const playButton = document.getElementById("playButton");
 
@@ -34,60 +35,72 @@ playButton.addEventListener('click', function() {
 
 }, false);
 
-
-// Viz stuff
+// Vizualization
 const canvas = document.getElementById("visualization");
 const size = {
   WIDTH: canvas.offsetWidth,
   HEIGHT: canvas.offsetHeight,
   RADIUS: 175
-}
-analyser.fftSize = 128;
-var bufferLength = analyser.frequencyBinCount;
-var dataArray = new Uint8Array(bufferLength);
-// var dataArray = new Float32Array(bufferLength);
+};
+const bufferLength = analyser.frequencyBinCount;
+const dataArray = new Uint8Array(bufferLength);
+const canvasCtx = canvas.getContext("2d");
 
-canvas.setAttribute('width', size.WIDTH)
+canvas.setAttribute('width', size.WIDTH);
 canvas.setAttribute('height', size.HEIGHT);
-var canvasCtx = canvas.getContext("2d");
-
-
 canvasCtx.clearRect(0, 0, size.WIDTH, size.HEIGHT);
 
-function draw() {
+var soundBooms = [],
+prevAvg;
+
+// draws canvas animations from audio analyser data
+const draw = () => {
+  // performs animations and passes draw() as callback for next repaint
   requestAnimationFrame(draw);
 
+  //
   analyser.getByteFrequencyData(dataArray);
 
   // build data
-  const lowFreqArray = dataArray.slice(8, 33);
+  const lowFreqArray = dataArray.slice(6, 35);
   const reversedArray = lowFreqArray.slice().reverse();
   const fullArray = [...new Array(8)].reduce((acc, n, i) => {
     return [...acc, ...(i % 2 ? lowFreqArray : reversedArray)]
   }, []);
-  // const fullArray = [...new Array(8)].reduce(acc => [...acc, ...lowFreqArray], []);
+
   const fullLength = fullArray.length;
   const avg = fullArray.reduce((acc, n) => acc+n, 0) / fullLength;
 
-  // gradient background
-  // var grd = canvasCtx.createLinearGradient(size.WIDTH/2, 0, size.WIDTH/2, size.HEIGHT);
-  // grd.addColorStop(0.00, 'rgba(0, 0, 0, 1.000)');
-  // grd.addColorStop(1.000, 'rgba(255, 100, 100, 1.000)');
-  canvasCtx.fillStyle = `rgb(0, 0, 0)`;
+  canvasCtx.fillStyle = 'rgb(0,0,0)';
 
   // draw canvas
   canvasCtx.fillRect(0, 0, size.WIDTH, size.HEIGHT);
   canvasCtx.translate(size.WIDTH/2, size.HEIGHT/2);
 
+  // create sounds boom ring
+  avg > 100 && avg > (prevAvg + 5) && soundBooms.push(size.RADIUS - (avg/3));
+  // animate sound booms
+  soundBooms = soundBooms.map((boomRadius) => {
+    canvasCtx.strokeStyle = `rgba(51, 51, 51, ${1-(boomRadius/500)}`;
+    canvasCtx.lineWidth = 8;
+    canvasCtx.beginPath();
+    canvasCtx.arc(0, 0, boomRadius, 0, Math.PI*2);
+    canvasCtx.fill();
+    canvasCtx.stroke();
+    return boomRadius < 500 && boomRadius + 2
+  }).filter(r => !!r);
+
+  prevAvg = avg;
+
   // draw frequency bars
   fullArray.map((n, i) => {
     canvasCtx.rotate((360/fullLength*1) * Math.PI / 180);
-    canvasCtx.fillStyle = `rgb(${n}, ${255 - n}, ${255 - n})`;
+    canvasCtx.fillStyle = `rgb(${n*1.5}, ${n/2}, ${255 - n})`;
     canvasCtx.fillRect(0, size.RADIUS - (avg/3), 2, n && n+50);
   })
 
   canvasCtx.setTransform(1, 0, 0, 1, 0, 0);
-
 }
 
+// initial draw
 draw();
